80eb0b8c7458038b5f4d859b3e4098d7184d45d5
xjz1994 <504767600@qq.com>
2017-12-31 17:07:24 +0800

    client

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-let WebsiteType = require("./src/EnumType").WebsiteType;
-let CityType = require("./src/EnumType").CityType;
-let Sender = require("./src/Sender").Sender;
-let Utils = require("./src/Utils").Utils;
-let fs = require("fs");
+// let WebsiteType = require("./server/EnumType").WebsiteType;
+// let CityType = require("./server/EnumType").CityType;
+// let Sender = require("./server/Sender").Sender;
+// let Parser = require("./server/Parser").Parser;
+// let Utils = require("./server/Utils").Utils;
+// let fs = require("fs");
 
+// let main = async () => {
+//     let city = CityType.GuangZhou;
+//     let searchKey = Utils.GetUrlCode(".net");
+//     let t1 = new Date().getTime();
 
-let main = async () => {
-    let city = CityType.GuangZhou;
-    let searchKey = Utils.GetUrlCode(".net");
+//     let lagouBody = await Sender.LaGou(1, city, searchKey);
+//     let lagouData = Parser.LaGou(lagouBody);
 
-    let lagouBody = await Sender.LaGou(1, city, searchKey);
-    let lagouData = Parser.LaGou(lagouBody);
+//     let qiancheng = await Sender.QianChengWuYou(1, city, searchKey);
+//     let qianChengData = await Parser.QianChengWuYou(qiancheng);
 
-    let qiancheng = await Sender.QianChengWuYou(1, city, searchKey);
-    let qianChengData = await Parser.QianChengWuYou(qiancheng);
+//     let zhilianBody = await Sender.ZhiLian(1, city, searchKey);
+//     let zhilianData = await Parser.ZhiLian(zhilianBody);
 
-    let zhilianBody = await Sender.ZhiLian(1, city, searchKey);
-    let zhilianData = await Parser.ZhiLian(zhilianBody);
+//     let liepin = await Sender.LiePin(1, city, searchKey);
+//     let liepinData = await Parser.LiePin(liepin);
 
-    let liepin = await Sender.LiePin(1, city, searchKey);
-    let liepinData = await Parser.LiePin(liepin);
+//     let boss = await Sender.BossZhiPin(1, city, searchKey);
+//     let bossData = await Parser.BossZhiPin(boss);
 
-    let boss = await Sender.BossZhiPin(1, city, searchKey);
-    let bossData = await Parser.BossZhiPin(boss);
+//     let positionData = lagouData.concat(zhilianData).concat(qianChengData).concat(liepinData).concat(bossData);
 
-    let positionData = lagouData.concat(zhilianData).concat(qianChengData).concat(liepinData).concat(bossData);
-    console.log(positionData);
-};
+//     let t2 = new Date().getTime();
 
-main();
+//     console.log(t2 - t1)
+//     console.log(positionData);
+// };
+
+// main();
+
+const Koa = require('koa');
+const serve = require('koa-static');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('api/search', async (ctx, next) => {
+    ctx.body = "search result";
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .use(serve(`${__dirname}/client/build`));
+
+app.listen(3000);
